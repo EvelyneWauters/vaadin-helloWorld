d99498b8131b0a94574b5073c3d5a40d67998e7d
EvelyneWauters <evelyne.wauters@gmail.com>
2015-07-08 10:12:13 +0200

    hello world class - setup

--- src/main/java/com/realdolmen/HelloWorld.java
+++ src/main/java/com/realdolmen/HelloWorld.java
@@ -1,7 +1,32 @@
 package com.realdolmen;
 
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.Title;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.annotations.Widgetset;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.UI;
+
+import javax.servlet.annotation.WebServlet;
+
 /**
  * Created by Evelyne on 08/07/15.
  */
-public class HelloWorld {
+
+@Title("Hello, world")
+@Theme("valo")
+@Widgetset("com.realdolmen.helloworld.MyAppWidgetSet")  //zie in resources
+public class HelloWorld extends UI {
+
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+
+    }
+
+    @WebServlet(value = "/*", asyncSupported = true)
+    @VaadinServletConfiguration(productionMode = false, ui = HelloWorld.class)  //doet al het werk, je moet wel zeggen welke ui de clientside moet inladen
+    public static class Servlet extends VaadinServlet   {
+
+    }
 }
